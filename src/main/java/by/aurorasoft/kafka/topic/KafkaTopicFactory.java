a765d631489710e3f08b62365f326d8c4360e67a
nikolaynn <kolya.by@mail.ru>
2021-12-06 17:19:50 +0300

    init commit

--- src/main/java/by/aurorasoft/kafka/topic/KafkaTopicFactory.java
+++ src/main/java/by/aurorasoft/kafka/topic/KafkaTopicFactory.java
@@ -4,7 +4,20 @@ import org.apache.kafka.clients.admin.NewTopic;
 
 public class KafkaTopicFactory {
 
-    public static NewTopic create(String name, int numberPartitions, short replicationFactor) {
-        return new NewTopic(name, numberPartitions, replicationFactor);
+    public static NewTopic create(String name, String numberPartitions, String replicationFactor) {
+
+        return new NewTopic(name, parseInt(name, numberPartitions), (short) parseInt(name, replicationFactor));
+    }
+
+    private static int parseInt(String name, String value) {
+        if (value == null) {
+            throw new IllegalArgumentException(String.format("Topic: '%s' creating failed. value is null", name));
+        }
+        try {
+            return Integer.parseInt(value);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
     }
 }
