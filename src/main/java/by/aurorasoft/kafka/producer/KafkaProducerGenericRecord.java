757b024140d89b18e101d1b221a24304e31b7568
nikolaynn <kolya.by@mail.ru>
2021-12-08 13:56:02 +0300

    implement KafkaProducerGenericRecordBase

--- src/main/java/by/aurorasoft/kafka/producer/KafkaProducerGenericRecord.java
+++ src/main/java/by/aurorasoft/kafka/producer/KafkaProducerGenericRecord.java
@@ -1,16 +1,12 @@
 package by.aurorasoft.kafka.producer;
 
 import org.apache.avro.Schema;
-import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.generic.GenericRecord;
-import org.apache.avro.io.*;
-import org.apache.avro.reflect.ReflectDatumWriter;
 import org.springframework.kafka.core.KafkaTemplate;
 
-import java.io.ByteArrayOutputStream;
 import java.util.Collection;
 
-public abstract class KafkaProducerGenericRecord<TOPIC_KEY, APP_MODEL, TRANSPORTABLE> extends KafkaProducerAbstract<TOPIC_KEY, GenericRecord, TRANSPORTABLE> {
+public abstract class KafkaProducerGenericRecord<TOPIC_KEY, APP_MODEL, TRANSPORTABLE> extends KafkaProducerGenericRecordBase<TOPIC_KEY, TRANSPORTABLE> {
 
     public KafkaProducerGenericRecord(String topicName, KafkaTemplate<TOPIC_KEY, GenericRecord> kafkaTemplate, Schema schema) {
         super(topicName, kafkaTemplate, schema);
@@ -36,23 +32,5 @@ public abstract class KafkaProducerGenericRecord<TOPIC_KEY, APP_MODEL, TRANSPORT
 
     protected boolean isSendable(TRANSPORTABLE transportable) {
         return transportable != null;
-    }
-
-    protected GenericRecord pojoToRecord(TRANSPORTABLE model) {
-        try {
-            ReflectDatumWriter<TRANSPORTABLE> datumWriter = new ReflectDatumWriter<>(schema);
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-
-            BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(outputStream, null);
-            datumWriter.write(model, encoder);
-            encoder.flush();
-
-            DatumReader<GenericRecord> datumReader = new GenericDatumReader<>(schema);
-            BinaryDecoder decoder = DecoderFactory.get().binaryDecoder(outputStream.toByteArray(), null);
-
-            return datumReader.read(null, decoder);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 }
---
+++ src/main/java/by/aurorasoft/kafka/producer/KafkaProducerGenericRecordBase.java
@@ -0,0 +1,36 @@
+package by.aurorasoft.kafka.producer;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.*;
+import org.apache.avro.reflect.ReflectDatumWriter;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Collection;
+
+public abstract class KafkaProducerGenericRecordBase<TOPIC_KEY, TRANSPORTABLE> extends KafkaProducerAbstract<TOPIC_KEY, GenericRecord, TRANSPORTABLE> {
+
+    public KafkaProducerGenericRecordBase(String topicName, KafkaTemplate<TOPIC_KEY, GenericRecord> kafkaTemplate, Schema schema) {
+        super(topicName, kafkaTemplate, schema);
+    }
+
+    protected GenericRecord pojoToRecord(TRANSPORTABLE model) {
+        try {
+            ReflectDatumWriter<TRANSPORTABLE> datumWriter = new ReflectDatumWriter<>(schema);
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+            BinaryEncoder encoder = EncoderFactory.get().binaryEncoder(outputStream, null);
+            datumWriter.write(model, encoder);
+            encoder.flush();
+
+            DatumReader<GenericRecord> datumReader = new GenericDatumReader<>(schema);
+            BinaryDecoder decoder = DecoderFactory.get().binaryDecoder(outputStream.toByteArray(), null);
+
+            return datumReader.read(null, decoder);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
