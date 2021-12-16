7fadb04deaf3a233b7a35eb075f9eb5a4527d1de
nikolaynn <kolya.by@mail.ru>
2021-12-16 15:46:04 +0300

    update kafkaProducer new class hierarchy update 2

---
+++ src/main/java/by/aurorasoft/kafka/producer/KafkaProducerGeneric.java
@@ -0,0 +1,16 @@
+package by.aurorasoft.kafka.producer;
+
+import org.apache.avro.Schema;
+import org.springframework.kafka.core.KafkaTemplate;
+
+public abstract class KafkaProducerGeneric<TOPIC_KEY, TOPIC_VALUE, MODEL> extends KafkaProducerAbstract<TOPIC_KEY, TOPIC_VALUE, MODEL, MODEL> {
+
+    public KafkaProducerGeneric(String topicName, KafkaTemplate<TOPIC_KEY, TOPIC_VALUE> kafkaTemplate, Schema schema) {
+        super(topicName, kafkaTemplate, schema);
+    }
+
+    @Override
+    protected MODEL convertModelToIntermediary(MODEL model) {
+        return model;
+    }
+}
