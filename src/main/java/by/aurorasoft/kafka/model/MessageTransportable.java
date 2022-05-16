88747ded197b3350fc23041713c5e6f2a57429ae
NikolayNN <nikolay.horushko@gmail.com>
2022-05-16 10:03:25 +0300

    update MessageTransportable

--- src/main/java/by/aurorasoft/kafka/model/MessageTransportable.java
+++ src/main/java/by/aurorasoft/kafka/model/MessageTransportable.java
@@ -1,92 +1,88 @@
 package by.aurorasoft.kafka.model;
 
+import java.time.Instant;
+
 public class MessageTransportable {
 
     private final Long id;
-    /** seconds */
-    private final long datetime;
+    private final Instant time;
     private final float latitude;
     private final float longitude;
     private final int altitude;
     private final int speed;
     private final int amountSatellite;
     private final int course;
+    /**
+     * kilometers
+     */
+    private final double gpsOdometer;
+    /**
+     * GSM level strength (Percent)
+     */
+    private final int gsmStrength;
+    /**
+     * tracker battery charge (Percent)
+     */
+    private final int battery;
+    /**
+     * tracker battery voltage (mV)
+     */
+    private final int batteryVoltage;
+
+    /**
+     * external power supply of the device (Volt)
+     */
+    private final float onboardVoltage;
+
+    /**
+     * acceleration of acceleration (G)
+     */
+    private final float ecoAcceleration;
+    /**
+     * acceleration of braking (G)
+     */
+    private final float ecoBraking;
+
+    /**
+     * cornering acceleration (G)
+     */
+    private final float ecoCornering;
+
+    /**
+     * bump acceleration (G)
+     */
+    private final float ecoBump;
+
     private final String params;
     private final long unitId;
     private final boolean isArchive;
     private final boolean isValid;
 
-    public MessageTransportable(Long id,
-                                long datetime,
-                                float latitude,
-                                float longitude,
-                                int altitude,
-                                int speed,
-                                int amountSatellite,
-                                int course,
-                                String params,
-                                long unitId,
-                                boolean isArchive,
-                                boolean isValid) {
+    public MessageTransportable(Long id, Instant time, float latitude, float longitude, int altitude, int speed,
+                                int amountSatellite, int course, double gpsOdometer, int gsmStrength, int battery,
+                                int batteryVoltage, float onboardVoltage,
+                                float ecoAcceleration, float ecoBraking, float ecoCornering, float ecoBump,
+                                String params, long unitId, boolean isArchive, boolean isValid) {
         this.id = id;
-        this.datetime = datetime;
+        this.time = time;
         this.latitude = latitude;
         this.longitude = longitude;
         this.altitude = altitude;
         this.speed = speed;
         this.amountSatellite = amountSatellite;
         this.course = course;
+        this.gpsOdometer = gpsOdometer;
+        this.gsmStrength = gsmStrength;
+        this.battery = battery;
+        this.batteryVoltage = batteryVoltage;
+        this.onboardVoltage = onboardVoltage;
+        this.ecoAcceleration = ecoAcceleration;
+        this.ecoBraking = ecoBraking;
+        this.ecoCornering = ecoCornering;
+        this.ecoBump = ecoBump;
         this.params = params;
         this.unitId = unitId;
         this.isArchive = isArchive;
         this.isValid = isValid;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public long getDatetime() {
-        return datetime;
-    }
-
-    public float getLatitude() {
-        return latitude;
-    }
-
-    public float getLongitude() {
-        return longitude;
-    }
-
-    public int getAltitude() {
-        return altitude;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    public int getAmountSatellite() {
-        return amountSatellite;
-    }
-
-    public int getCourse() {
-        return course;
-    }
-
-    public String getParams() {
-        return params;
-    }
-
-    public long getUnitId() {
-        return unitId;
-    }
-
-    public boolean isArchive() {
-        return isArchive;
-    }
-
-    public boolean isValid() {
-        return isValid;
     }
 }
