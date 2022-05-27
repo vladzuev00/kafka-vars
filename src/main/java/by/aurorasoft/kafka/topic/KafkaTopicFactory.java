cd1bd1504502f5bc3a8bde20fbfaedb70ff0327e
NikolayNN <nikolay.horushko@gmail.com>
2022-05-27 09:25:27 +0300

    create topic with retentions

--- src/main/java/by/aurorasoft/kafka/topic/KafkaTopicFactory.java
+++ src/main/java/by/aurorasoft/kafka/topic/KafkaTopicFactory.java
@@ -1,8 +1,12 @@
 package by.aurorasoft.kafka.topic;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
 
 public class KafkaTopicFactory {
+
+    private static final int DAY_MS = 86_400_000;
+    private static final long BYTES_GB = 1073741824;
 
     /**
      *     @Value(value = "${kafka.topic.received-commands.partitions-number}")
@@ -15,10 +19,29 @@ public class KafkaTopicFactory {
      *         return KafkaTopicFactory.create(KafkaVars.RECEIVED_COMMANDS_LOG_TOPIC_NAME, receivedCommandsPartitionsNumber, receivedCommandsReplicationFactor);
      *     }
      *
+     *     0 - kafka default value
+     *
      */
+    public static NewTopic create(String name, String numberPartitions, String replicationFactor, int retentionDays, int retentionGb) {
+        NewTopic topic = new NewTopic(name, parseInt(name, numberPartitions), (short) parseInt(name, replicationFactor));
+        if (retentionDays > 0) {
+            topic.configs().put(TopicConfig.RETENTION_MS_CONFIG, String.valueOf(retentionDays * DAY_MS));
+        }
+        if (retentionDays == -1) {
+            topic.configs().put(TopicConfig.RETENTION_MS_CONFIG, String.valueOf(retentionDays));
+        }
+        if (retentionGb > 0) {
+            topic.configs().put(TopicConfig.RETENTION_MS_CONFIG, String.valueOf(retentionGb * BYTES_GB));
+        }
+        if (retentionGb == -1) {
+            topic.configs().put(TopicConfig.RETENTION_MS_CONFIG, String.valueOf(retentionGb));
+        }
+        return new NewTopic(name, parseInt(name, numberPartitions), (short) parseInt(name, replicationFactor));
+    }
+
     public static NewTopic create(String name, String numberPartitions, String replicationFactor) {
 
-        return new NewTopic(name, parseInt(name, numberPartitions), (short) parseInt(name, replicationFactor));
+        return create(name, numberPartitions, replicationFactor, 0, 0);
     }
 
     private static int parseInt(String name, String value) {
