9c13d18958fd8edbf4a33b3afdf2172990f12be3
NikolayNN <nikolay.horushko@gmail.com>
2022-06-13 15:53:45 +0300

    new implementation KafkaProducerGenericRecordIntermediaryHooks

---
+++ src/main/java/by/aurorasoft/kafka/producer/KafkaProducerGenericRecordIntermediaryHooks.java
@@ -0,0 +1,54 @@
+package by.aurorasoft.kafka.producer;
+
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericRecord;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.lang.Nullable;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class KafkaProducerGenericRecordIntermediaryHooks<TOPIC_KEY, TRANSPORTABLE, MODEL> extends KafkaProducerGenericRecordIntermediary<TOPIC_KEY, TRANSPORTABLE, MODEL> {
+
+    private final List<MODEL> failureMessages = Collections.synchronizedList(new ArrayList<>());
+    public final ProducerMetrics metrics = new ProducerMetrics();
+
+    public KafkaProducerGenericRecordIntermediaryHooks(String topicName, KafkaTemplate<TOPIC_KEY, GenericRecord> kafkaTemplate, Schema schema) {
+        super(topicName, kafkaTemplate, schema);
+    }
+
+    @Override
+    public void send(MODEL model) {
+        onBeforeSend();
+        sendModel(getTopicKey(model), model).addCallback(new ListenableFutureCallback<>() {
+
+            @Override
+            public void onFailure(@Nullable Throwable throwable) {
+                failureMessages.add(model);
+                metrics.failureCounterIncrement();
+                onSendFailure(model, throwable);
+            }
+
+            @Override
+            public void onSuccess(@Nullable SendResult<TOPIC_KEY, GenericRecord> sendResult) {
+                metrics.successCounterIncrement();
+                onSendSuccess(sendResult);
+            }
+        });
+        metrics.sentCounterIncrement();
+    }
+
+    protected abstract TOPIC_KEY getTopicKey(MODEL model);
+
+    protected void onBeforeSend() {
+    }
+
+    protected void onSendFailure(MODEL model, Throwable throwable) {
+    }
+
+    protected void onSendSuccess(@Nullable SendResult<TOPIC_KEY, GenericRecord> sendResult) {
+    }
+}
---
+++ src/main/java/by/aurorasoft/kafka/producer/ProducerMetrics.java
@@ -0,0 +1,44 @@
+package by.aurorasoft.kafka.producer;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Getter
+@AllArgsConstructor
+public class ProducerMetrics {
+    private final AtomicLong sentCounter;
+    private final AtomicLong successCounter;
+    private final AtomicLong failureCounter;
+
+    public ProducerMetrics() {
+        sentCounter = new AtomicLong(0);
+        successCounter = new AtomicLong(0);
+        failureCounter = new AtomicLong(0);
+    }
+
+    public void sentCounterIncrement() {
+        sentCounter.incrementAndGet();
+    }
+
+    public void successCounterIncrement() {
+        successCounter.incrementAndGet();
+    }
+
+    public void failureCounterIncrement() {
+        failureCounter.incrementAndGet();
+    }
+
+    public long getNotApprovedCount() {
+        return (sentCounter.get() - (successCounter.get() + failureCounter.get()));
+    }
+
+    public ProducerMetrics instance(){
+        return new ProducerMetrics(
+                new AtomicLong(sentCounter.get()),
+                new AtomicLong(successCounter.get()),
+                new AtomicLong(failureCounter.get())
+        );
+    }
+}
