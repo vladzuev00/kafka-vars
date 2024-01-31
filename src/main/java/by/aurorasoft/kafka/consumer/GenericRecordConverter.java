436807d76c8d4d4842ee30f9e891df3031fc8f1b
Nikolay <nikolay.horushko@gmail.com>
2024-01-31 13:36:46 +0300

    GenericRecordConverter

---
+++ src/main/java/by/aurorasoft/kafka/consumer/GenericRecordConverter.java
@@ -0,0 +1,43 @@
+package by.aurorasoft.kafka.consumer;
+
+import org.apache.avro.generic.GenericRecord;
+
+import java.time.Instant;
+
+public interface GenericRecordConverter {
+    default long getLong(GenericRecord record, String name) {
+        return (long) record.get(name);
+    }
+
+    default int getInt(GenericRecord record, String name) {
+        return (int) record.get(name);
+    }
+
+    default long getLongObj(GenericRecord record, String name) {
+        return (Long) record.get(name);
+    }
+
+    default float getFloat(GenericRecord record, String name) {
+        return (float) record.get(name);
+    }
+
+    default double getDouble(GenericRecord record, String name) {
+        return (double) record.get(name);
+    }
+
+    default String getString(GenericRecord record, String name) {
+        return record.get(name).toString();
+    }
+
+    default boolean getBoolean(GenericRecord record, String name) {
+        return (boolean) record.get(name);
+    }
+
+    default Instant getInstant(GenericRecord record, String name) {
+        return Instant.ofEpochSecond((long) record.get(name));
+    }
+
+    default <T> T getObject(GenericRecord record, String name) {
+        return (T) record.get(name);
+    }
+}
--- src/main/java/by/aurorasoft/kafka/consumer/KafkaConsumerGenericRecord.java
+++ src/main/java/by/aurorasoft/kafka/consumer/KafkaConsumerGenericRecord.java
@@ -3,9 +3,9 @@ package by.aurorasoft.kafka.consumer;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
-import java.time.Instant;
-
-public abstract class KafkaConsumerGenericRecord<TOPIC_KEY, POJO> extends KafkaConsumerAbstract<TOPIC_KEY, GenericRecord> {
+public abstract class KafkaConsumerGenericRecord<TOPIC_KEY, POJO>
+        extends KafkaConsumerAbstract<TOPIC_KEY, GenericRecord>
+        implements GenericRecordConverter {
 
     protected POJO map(ConsumerRecord<TOPIC_KEY, GenericRecord> consumerRecord) {
         return map(consumerRecord.value());
@@ -13,39 +13,4 @@ public abstract class KafkaConsumerGenericRecord<TOPIC_KEY, POJO> extends KafkaC
 
     protected abstract POJO map(GenericRecord record);
 
-    protected long getLong(GenericRecord record, String name) {
-        return (long) record.get(name);
-    }
-
-    protected int getInt(GenericRecord record, String name) {
-        return (int) record.get(name);
-    }
-
-    protected long getLongObj(GenericRecord record, String name) {
-        return (Long) record.get(name);
-    }
-
-    protected float getFloat(GenericRecord record, String name) {
-        return (float) record.get(name);
-    }
-
-    protected double getDouble(GenericRecord record, String name) {
-        return (double) record.get(name);
-    }
-
-    protected String getString(GenericRecord record, String name) {
-        return record.get(name).toString();
-    }
-
-    protected boolean getBoolean(GenericRecord record, String name) {
-        return (boolean) record.get(name);
-    }
-
-    protected Instant getInstant(GenericRecord record, String name) {
-        return Instant.ofEpochSecond((long) record.get(name));
-    }
-
-    protected <T> T getObject(GenericRecord record, String name) {
-        return (T)record.get(name);
-    }
 }
