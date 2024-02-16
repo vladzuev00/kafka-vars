85e9bd75046d83c21826daa5fd7f256031d7e50e
Vlad Zuev <vladzuev.00@mail.ru>
2024-02-16 10:36:54 +0300

    refactor replication producing

--- src/main/java/by/aurorasoft/kafka/replication/model/replication/Replication.java
+++ src/main/java/by/aurorasoft/kafka/replication/model/replication/Replication.java
@@ -1,15 +1,17 @@
 package by.aurorasoft.kafka.replication.model.replication;
 
-import by.aurorasoft.kafka.replication.model.TransportableReplication;
-import by.aurorasoft.kafka.replication.model.TransportableReplication.ReplicationType;
-import by.aurorasoft.kafka.replication.producer.ReplicatedDtoSerializer;
+import by.aurorasoft.kafka.replication.model.ReplicationType;
 import by.nhorushko.crudgeneric.v2.domain.AbstractDto;
 import by.nhorushko.crudgeneric.v2.service.AbsServiceCRUD;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
 @RequiredArgsConstructor
+@Getter
 @EqualsAndHashCode
+@ToString
 public abstract class Replication<ID, DTO extends AbstractDto<ID>> {
     private final DTO dto;
 
@@ -17,17 +19,11 @@ public abstract class Replication<ID, DTO extends AbstractDto<ID>> {
         return dto.getId();
     }
 
-    public final TransportableReplication createTransportable(final ReplicatedDtoSerializer<ID, DTO> serializer) {
-        final ReplicationType type = getType();
-        final String dtoJson = serializer.serialize(dto);
-        return new TransportableReplication(type, dtoJson);
-    }
+    public abstract ReplicationType getType();
 
     public final void execute(final AbsServiceCRUD<ID, ?, DTO, ?> service) {
         execute(service, dto);
     }
 
     protected abstract void execute(final AbsServiceCRUD<ID, ?, DTO, ?> service, final DTO dto);
-
-    protected abstract ReplicationType getType();
 }
--- src/main/java/by/aurorasoft/kafka/replication/producer/KafkaProducerReplication.java
+++ src/main/java/by/aurorasoft/kafka/replication/producer/KafkaProducerReplication.java
@@ -4,6 +4,7 @@ import by.aurorasoft.kafka.producer.KafkaProducerGenericRecordIntermediaryHooks;
 import by.aurorasoft.kafka.replication.model.TransportableReplication;
 import by.aurorasoft.kafka.replication.model.replication.Replication;
 import by.nhorushko.crudgeneric.v2.domain.AbstractDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericRecord;
@@ -11,14 +12,14 @@ import org.springframework.kafka.core.KafkaTemplate;
 
 public abstract class KafkaProducerReplication<ID, DTO extends AbstractDto<ID>>
         extends KafkaProducerGenericRecordIntermediaryHooks<ID, TransportableReplication, Replication<ID, DTO>> {
-    private final ReplicatedDtoSerializer<ID, DTO> dtoSerializer;
+    private final ObjectMapper objectMapper;
 
     public KafkaProducerReplication(final String topicName,
                                     final KafkaTemplate<ID, GenericRecord> kafkaTemplate,
                                     final Schema schema,
                                     final ObjectMapper objectMapper) {
         super(topicName, kafkaTemplate, schema);
-        dtoSerializer = new ReplicatedDtoSerializer<>(objectMapper);
+        this.objectMapper = objectMapper;
     }
 
     @Override
@@ -28,6 +29,33 @@ public abstract class KafkaProducerReplication<ID, DTO extends AbstractDto<ID>>
 
     @Override
     protected final TransportableReplication convertModelToTransportable(final Replication<ID, DTO> replication) {
-        return replication.createTransportable(dtoSerializer);
+        try {
+            final String dtoJson = objectMapper.writeValueAsString(replication.getDto());
+            return new TransportableReplication(replication.getType(), dtoJson);
+        } catch (final JsonProcessingException cause) {
+            throw new ReplicationProducingException(cause);
+        }
+    }
+
+    private static final class ReplicationProducingException extends RuntimeException {
+
+        @SuppressWarnings("unused")
+        public ReplicationProducingException() {
+
+        }
+
+        @SuppressWarnings("unused")
+        public ReplicationProducingException(final String description) {
+            super(description);
+        }
+
+        public ReplicationProducingException(final Exception cause) {
+            super(cause);
+        }
+
+        @SuppressWarnings("unused")
+        public ReplicationProducingException(final String description, final Exception cause) {
+            super(description, cause);
+        }
     }
 }
